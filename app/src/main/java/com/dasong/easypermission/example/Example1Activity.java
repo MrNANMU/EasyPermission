6f810c0232c5e187b50f7cac67e15495a0bf564b
18050009 <zhengxingda8605@dingtalk.com>
2019-12-23 17:32:07 +0800

    完善

--- app/src/main/java/com/dasong/easypermission/example/Example1Activity.java
+++ app/src/main/java/com/dasong/easypermission/example/Example1Activity.java
@@ -1,9 +1,11 @@
 package com.dasong.easypermission.example;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,6 +24,12 @@ public class Example1Activity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EasyPermission.init(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermission.onRequestPermissionsResult(requestCode,permissions,grantResults);
     }
 
     @BeforeRequest
