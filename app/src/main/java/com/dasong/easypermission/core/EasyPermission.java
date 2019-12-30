cc68767eec2c0053c207404ed571e852bfb60711
18050009 <zhengxingda8605@dingtalk.com>
2019-12-30 17:29:31 +0800

    新构思

--- app/src/main/java/com/dasong/easypermission/core/EasyPermission.java
+++ app/src/main/java/com/dasong/easypermission/core/EasyPermission.java
@@ -1,14 +1,30 @@
 package com.dasong.easypermission.core;
 
 import android.app.Activity;
+import android.os.Build;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.collection.ArrayMap;
 
 import java.util.Map;
 
 public class EasyPermission {
 
-    private static Map<String,Poster> cache;
+    private EasyPermission mEp;
+    private Poster mPoster;
+
+    private EasyPermission(){
+
+    }
+
+    private static EasyPermission getInstance(){
+        return EPInstance.instance;
+    }
+
+    private static class EPInstance{
+        private static final EasyPermission instance = new EasyPermission();
+    }
 
     /**
      * {@link #init(Activity)}总是和{@link #post()}或{@link #post(String...)}配合使用。
@@ -17,41 +33,27 @@ public class EasyPermission {
      * @param activity
      */
     public static void init(Activity activity){
-        if(cache == null){
-            cache = new ArrayMap<>();
-        }
-        if(cache.get(activity.getClass().getName()) == null){
-            Poster poster = new Poster(Poster.MODE_LASY,activity);
-            cache.put(activity.getClass().getSimpleName(),poster);
-        }
+        getInstance().mPoster = new Poster(Poster.MODE_LASY,activity);
     }
 
-    public static void post(Activity activity){
-        Poster poster = cache.get(activity.getClass().getName());
-        if(poster != null){
-            poster.request();
-        }
+    public static void post(){
+
     }
 
-    public static void post(Activity activity,String...permissions){
-        Poster poster = cache.get(activity.getClass().getName());
-        if(poster != null){
-            poster.request(permissions);
-        }
+    public static void post(String...permissions){
+
     }
 
     public static void request(Activity activity){
-        if(cache == null){
-            cache = new ArrayMap<>();
-        }
-        Poster poster = new Poster(Poster.MODE_LASY,activity);
-        poster.request();
+        getInstance().mPoster = new Poster(Poster.MODE_REQUEST,activity);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public static void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
     }
 
     public static void clear(Activity activity){
-        Poster poster = cache.get(activity.getClass().getName());
-        if(poster != null){
-            poster.clear();
-        }
+
     }
 }
